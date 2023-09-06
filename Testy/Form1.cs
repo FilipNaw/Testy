7809b5326c9ebd37a4b3b07454b3764d59a117bc
Filip Nawojczyk <filip.nawojczyk@teb.edu.pl>
2023-09-06 12:39:36 +0200

    Button move

--- Testy/Form1.cs
+++ Testy/Form1.cs
@@ -6,10 +6,23 @@ namespace Testy
         {
             InitializeComponent();
         }
-
         private void ButtonClicked(object sender, EventArgs e)
         {
-            Application.Exit();
+            //Application.Exit();
+
+            int windowWidth = Size.Width;
+            int windowHeight = Size.Height;
+
+            windowWidth -= AAA.Width;
+            windowHeight -= AAA.Height;
+
+            Random rnd = new Random();
+
+            int randomLeft = rnd.Next(windowWidth);
+            int randomTop = rnd.Next(windowHeight);
+
+            Point topLeftCorner = new Point(randomLeft, randomTop);
+            AAA.Location = topLeftCorner;   
         }
     }
 }
